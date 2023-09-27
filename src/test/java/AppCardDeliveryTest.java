3fe6cc5706642df100c129a3343f6ac96ae88d9a
Vladimir Zakharov <vldzhv@gmail.com>
2023-09-27 15:50:50 +0300

    Initial commit

---
+++ src/test/java/AppCardDeliveryTest.java
@@ -0,0 +1,38 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.files.DownloadActions.click;
+import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
+
+public class AppCardDeliveryTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+    @Test
+    public void shouldBeSuccessful() {
+        Configuration.timeout = 15;
+        open("http://localhost:9999");
+        $("[data-test-id='city'] input").setValue("Москва");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(currentDate);
+        $("[data-test-id='name'] input").setValue("Иванов Иван Иванович");
+        $("[data-test-id='phone'] input").setValue("+71231231212");
+        $("[data-test-id='agreement']").click();
+        $("notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+
+    }
+
+}
